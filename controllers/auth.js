7f5a80e5968f37e5c417028c35695809d16661ac
Juan Hernandez Puebla <al222010053@gmail.com>
2024-04-17 19:27:39 -0600

    Creacion de ruta login/google para acceso/creacion de usuario via Google Sign in

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const Usuario = require("../models/usuario");
 const bcrypt = require("bcryptjs");
 const { generarJWT } = require("../helpers/jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
   const { email, password } = req.body;
@@ -42,6 +43,50 @@ const login = async (req, res = response) => {
   }
 };
 
+const googleSignIn = async (req, res = response) => {
+  const googleToken = req.body.token;
+
+  try {
+    const { name, email, picture } = await googleVerify(googleToken);
+
+    const usuarioDB = await Usuario.findOne({ email });
+    let usuario;
+
+    // Si no existe el usuario se debe crear
+    if (!usuarioDB) {
+      usuario = new Usuario({
+        nombre: name,
+        email: email,
+        password: "####",
+        img: picture,
+        google: true,
+      });
+    } else {
+      //existe el usuario
+      usuario = usuarioDB;
+      usuario.google = true;
+    }
+
+    // Guardar en DB
+
+    await usuario.save();
+
+    // Generar JWT con funcion helpers/jwt/generarJWT()
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      ok: true,
+      token,
+    });
+  } catch (error) {
+    res.status(401).json({
+      ok: false,
+      msg: "Token no es correcto",
+    });
+  }
+};
+
 module.exports = {
   login,
+  googleSignIn,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 // Ruta /api/login
 const { Router } = require("express");
-const { login } = require("../controllers/auth");
+const { login, googleSignIn } = require("../controllers/auth");
 const { check } = require("express-validator");
 const { validarCampos } = require("../middlewares/validar-campos");
 
@@ -14,6 +14,12 @@ router.post(
     validarCampos,
   ],
   login
+);
+
+router.post(
+  "/google",
+  [check("token", "El token de Google").not().isEmpty(), validarCampos],
+  googleSignIn
 );
 
 module.exports = router;
